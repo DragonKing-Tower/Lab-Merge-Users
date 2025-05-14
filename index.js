bab6432419bae99dfa966d8b0934af38d5974a83
Cyril Rafalovich <replanttrees@gmail.com>
2025-05-14 07:48:56 -1000

    This is the Module 5 LAB

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require("datejs");
 
-
+function combineUsers(...args) {
+	const combinedObject = {
+		users: [],
+	};
+	args.forEach((array) => combinedObject.users.push(...array));
+	combinedObject.merge_date = Date.parse("today").toString("M/d/yyyy");
+	return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+	...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
